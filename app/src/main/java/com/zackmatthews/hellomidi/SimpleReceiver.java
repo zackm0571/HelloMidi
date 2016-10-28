60751d6332d6c28aa02fa703fa748d8bb5ce54d7
Zack Matthews <zack@zackmatthews.com>
2016-10-28 15:50:40 -0700

    Running tasker tasks!

--- app/src/main/java/com/zackmatthews/hellomidi/SimpleReceiver.java
+++ app/src/main/java/com/zackmatthews/hellomidi/SimpleReceiver.java
@@ -1,5 +1,6 @@
 package com.zackmatthews.hellomidi;
 
+import android.content.Context;
 import android.media.midi.MidiReceiver;
 
 import java.io.IOException;
@@ -10,9 +11,10 @@ import java.io.IOException;
 
 public class SimpleReceiver extends MidiReceiver {
     private MidiHelper.MidiHelperEventListener mListener;
-
-    public SimpleReceiver(MidiHelper.MidiHelperEventListener listener){
+    private Context context;
+    public SimpleReceiver(Context context, MidiHelper.MidiHelperEventListener listener){
         this.mListener = listener;
+        this.context = context;
     }
     @Override
     public void onSend(byte[] msg, int offset, int count, long timestamp) throws IOException {
@@ -20,7 +22,11 @@ public class SimpleReceiver extends MidiReceiver {
         if(msg[offset] != EventTriggerHelper.EMPTY && count > 1) {
             mListener.onMidiHelperStatusEvent(String.format(debug_msg, msg[offset+1], offset, count, timestamp));
         }
-        EventTriggerHelper.instance().evaluateInputEvent(msg[offset]);
+        //EventTriggerHelper.instance().evaluateInputEvent(msg[offset]);
+        if(msg[offset] == EventTriggerHelper.NOTE_ON) {
+            TaskerIntent i = new TaskerIntent("Test");
+            context.sendBroadcast(i);
+        }
 
 //        if(msg[offset] == EventTriggerHelper.NOTE_ON) { //NOTE_ON event
 //            mListener.onMidiHelperStatusEvent(EventTriggerHelper.LAUNCH_APP_EVENT_TRIGGER);
